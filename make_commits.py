7fcd081ac91fc28fa3c2bd2f122fb5fe5fa832ce
Alex <36340482+AlexSmith11@users.noreply.github.com>
2022-01-11 00:29:51 +0000

    Update make_commits.py

--- make_commits.py
+++ make_commits.py
@@ -26,6 +26,6 @@ messages = [
 ]
 
 # I couldn't get the bash for loop to run in the github action, so... here we are.
-for n in range(random.randint(1, 10)):
+for n in range(random.randint(1, 15)):
     message = q(random.choice(messages))
     os.system(f'git commit -m "{message}" --allow-empty')
